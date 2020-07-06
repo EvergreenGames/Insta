b3fe1c44e58517f11e76eb72201f7debe9748d12
Ruben Green <ragreen@fb.com>
2020-07-06 13:36:18 -0700

    Initial Commit

---
+++ Insta/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Insta
+//
+//  Created by Ruben Green on 7/6/20.
+//  Copyright © 2020 Ruben Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Insta/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Insta
+//
+//  Created by Ruben Green on 7/6/20.
+//  Copyright © 2020 Ruben Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
